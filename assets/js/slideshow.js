1aaeaa99683b461b9b85835ef7769e41365cc354
PaulBichler <Paulbichler@hotmail.de>
2021-07-19 12:01:01 +0200

    - Added Blogic and Protect the Heart - Removed Culth, Wemob Runner and Trajectory Calculator (Outdated)

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,5 +1,5 @@
-    var slideIndex = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-    var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10"];
+    var slideIndex = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11", "mySlides12"];
     showDivs(1, 0);
     showDivs(1, 1);
     showDivs(1, 2);
@@ -10,6 +10,8 @@
     showDivs(1, 7);
     showDivs(1, 8);
     showDivs(1, 9);
+    showDivs(1, 10);
+    showDivs(1, 11);
     
     function plusDivs(n, no) {
       showDivs(slideIndex[no] += n, no);
@@ -18,6 +20,10 @@
     function showDivs(n, no) {
       var i;
       var x = document.getElementsByClassName(slideId[no]);
+
+      if(x.length == 0)
+        return;
+
       if (n > x.length) {
         slideIndex[no] = 1
       }
